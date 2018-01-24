04ce88329fa752068078a503e2c45fd6209ce352
amir <amirdajanovic1983@gmail.com>
2018-01-24 20:31:23 +0100

    radni fajlovi

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ajax</title>
+</head>
+<body>
+	<a href="">Naslovna</a>
+	<a href="">O nama</a>
+	<a href="">Kontakt</a>
+
+
+<?php
+
+$strane = [
+	"1"=>"naslovna.php",
+	"2"=>"onama.php",
+	"3"=>"kontakt.php"
+];
+
+?>
+
+</body>
+</html>
